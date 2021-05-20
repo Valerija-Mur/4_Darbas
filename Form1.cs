76db8edd8d9b635271c76aea1d0acb7ebdbd83f9
Valerija-Mur <65787413+Valerija-Mur@users.noreply.github.com>
2021-05-20 16:22:16 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _4_darbas
+{
+    public partial class Form1 : Form
+    {
+        string password = "Test";
+        string path = "C:\\Users\\Hunter\\Desktop\\IS\\Testinis Folderis";
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+
+                if (File.Exists(path + "\\Magic.txt.aes"))
+                {
+                    Decrypt(path + "\\Magic.txt.aes", password);
+                    File.Delete(path + "\\Magic.txt.aes");
+                }
+                else
+                {
+                    File.Create(path + "\\Magic.txt").Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Encrypt(path + "\\Magic.txt", password);
+                File.Delete(path + "\\Magic.txt");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private void Encrypt(string inputFile, string password)
+        {
+            //generate random salt
+            byte[] salt = GenerateRandomSalt();
+
+            //create output file name
+            using FileStream fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create);
+
+            //convert password string to byte arrray
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            //Set Rijndael symmetric encryption algorithm
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            //"What it does is repeatedly hash the user password along with the salt." High iteration counts.
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            AES.Mode = CipherMode.CFB;
+
+            // write salt to the begining of the output file, so in this case can be random every time
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            //create a buffer (1mb) so only this amount will allocate in the memory and not the whole file
+            byte[] buffer = new byte[1048576];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents(); // -> for responsive GUI, using Task will be better!
+                    cs.Write(buffer, 0, read);
+                }
+
+                // Close up
+                fsIn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                cs.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        private void Decrypt(string inputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CFB;
+            string outputFile = inputFile.Substring(0, inputFile.Length - 4);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents();
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+        [DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
+        public static extern bool ZeroMemory(IntPtr Destination, int Length);
+
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(Comment_txt.Text) &&
+                !String.IsNullOrWhiteSpace(Name_txt.Text) &&
+                !String.IsNullOrWhiteSpace(Password_txt.Text) &&
+                !String.IsNullOrWhiteSpace(URL_App_txt.Text)
+                )
+            {
+                using (StreamWriter w = File.AppendText(path + "\\Magic.txt"))
+                {
+                    w.WriteLine(String.Format("{0};{1};{2};{3}", Name_txt.Text.ToString(), EncryptText(Password_txt.Text.ToString(), Encoding.UTF8.GetBytes("ananasasananasas")), URL_App_txt.Text.ToString(), Comment_txt.Text.ToString()));
+                    MessageBox.Show("sėkmingai išsaugota");
+                    Comment_txt.Text = "";
+                    Name_txt.Text = "";
+                    Password_txt.Text = "";
+                    URL_App_txt.Text = "";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Visi langai turi būti užpildyti");
+            }
+        }
+
+        private string EncryptText(string text, byte[] key)
+        {
+            // Nesifruotas tekstaspaverciamas i baitus
+            byte[] tekstas = Encoding.UTF8.GetBytes(text);
+            RijndaelManaged aes = new RijndaelManaged();
+            // Modas ecb
+            aes.Mode = CipherMode.ECB;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.KeySize = 128;
+
+            // Sukuria sifratoriu
+            using (ICryptoTransform sifratorius = aes.CreateEncryptor(key, null))
+            {
+                // Sifruojam teksta
+                byte[] sifruotasTekstas = sifratorius.TransformFinalBlock(tekstas, 0, tekstas.Length);
+                // Nutraukiam darba
+                sifratorius.Dispose();
+                // Grazinam sifruota teksta string formatu
+                return Convert.ToBase64String(sifruotasTekstas);
+            }
+        }
+        private string DecryptText(string text, byte[] key)
+        {
+            // Konvertuoja teksta i baitus
+            byte[] sifruotasTekstas = Convert.FromBase64String(text);
+            RijndaelManaged aes = new RijndaelManaged();
+            // Nustatom rakto dydi
+            aes.KeySize = 128;
+            aes.Padding = PaddingMode.PKCS7;
+            // Nuastatom moda i ecb
+            aes.Mode = CipherMode.ECB;
+
+            // Sukuria desifratoriu
+            using (ICryptoTransform desifratorius = aes.CreateDecryptor(key, null))
+            {
+                byte[] desifruotasTekstas = desifratorius.TransformFinalBlock(sifruotasTekstas, 0, sifruotasTekstas.Length);
+                // Nutraukia desifravimo darba
+                desifratorius.Dispose();
+                // Grazinam Desifruota teksta string formatu
+                return Encoding.UTF8.GetString(desifruotasTekstas);
+            }
+        }
+
+        private void FindName_btn_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            string line;
+            using StreamReader file = new StreamReader(path+"\\Magic.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] split = line.Split(';');
+                if (split[0] == FindName_txt.Text)
+                {
+                    ListViewItem lv = new ListViewItem(split[0]);
+                    lv.SubItems.Add(DecryptText(split[1], Encoding.UTF8.GetBytes("ananasasananasas")));
+                    lv.SubItems.Add(split[2]);
+                    lv.SubItems.Add(split[3]);
+                    listView1.Items.Add(lv);
+                }
+            }
+        }
+    }
+
+    class Password
+    {
+        public string name;
+        public string password;
+        public string url;
+        public string comment;
+
+
+        public Password(string name, string password, string url, string comment)
+        {
+            this.name = name;
+            this.password = password;
+            this.url = url;
+            this.comment = comment;
+        }
+    }
+}
